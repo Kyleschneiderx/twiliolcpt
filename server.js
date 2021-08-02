e3dc04863c62e09d5ace07618cdde3c2f66793cb
kyleschneiderx <kyleschneiderx@gmail.com>
2021-08-02 08:57:40 -0700

    add call forwarding

--- server.js
+++ server.js
@@ -28,14 +28,19 @@ app.use(urlencoded({ extended: false }));
 app.post('/voice', (req, response) => {
   // Use the Twilio Node.js SDK to build an XML response
   console.log(req.body.Caller)
-
+  const VoiceResponse = require('twilio').twiml.VoiceResponse;
 //  '<Response><Say>Hi Greg this is an automated phone message from Lake City Physical Therapy just letting you know that you have an open balance. Please at your earliest convience give our  billing department a call at 208-667-1988 or visit our website at lake-city-pt.com and navigate to the patient payment portal. If this information seems incorrect please contact us to resolve the matter, Thank You</Say></Response>'
-  const twiml = new VoiceResponse();
-  twiml.say(`Hi Kyle this is an automated phone message from Lake City Physical Therapy just letting you know that you have an outstanding balance. Please at your earliest convience give our  billing department a call at 208-667-1988 or visit our website at lake-city-pt.com and navigate to the patient payment portal. If this information seems incorrect please contact us to resolve the matter, Thank You and have a nice day.`);
+  // const twiml = new VoiceResponse();
+  const response = new VoiceResponse();
 
-  // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(twiml.toString());
+  response.dial('208-818-3015')
+  console.log(response.toString());
+
+  // twiml.say(`Hi Kyle this is an automated phone message from Lake City Physical Therapy just letting you know that you have an outstanding balance. Please at your earliest convience give our  billing department a call at 208-667-1988 or visit our website at lake-city-pt.com and navigate to the patient payment portal. If this information seems incorrect please contact us to resolve the matter, Thank You and have a nice day.`);
+
+  // // Render the response as XML in reply to the webhook request
+  // response.type('text/xml');
+  // response.send(twiml.toString());
 });
 
 
