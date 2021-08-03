e678497c290fc5f9823664e843c6fc683a62e4a8
kyleschneiderx <kyleschneiderx@gmail.com>
2021-08-03 05:57:01 -0700

    added in config and fixed number issues

--- server.js
+++ server.js
@@ -4,49 +4,70 @@ const urlencoded = require('body-parser').urlencoded;
 const mongoose = require('mongoose')
 const app = express();
 const {Patient} = require('./patient')
-
+require('dotenv').config()
 
 
 // O5ZO2nHPj09LAkO3
 
-mongoose.connect('mongodb+srv://kyleschneider:O5ZO2nHPj09LAkO3@cluster0.7rsp0.mongodb.net/twiliolcpt?retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGO_URI,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
 
-
-
-
-
 // Parse incoming POST params with Express middleware
 app.use(urlencoded({ extended: false }));
+
+app.use(express.json({
+  type: ['application/json', 'text/plain']
+}));
 
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
 app.post('/voice', (req, response) => {
-  // Use the Twilio Node.js SDK to build an XML response
-  console.log(req.body.Caller)
-  const VoiceResponse = require('twilio').twiml.VoiceResponse;
-//  '<Response><Say>Hi Greg this is an automated phone message from Lake City Physical Therapy just letting you know that you have an open balance. Please at your earliest convience give our  billing department a call at 208-667-1988 or visit our website at lake-city-pt.com and navigate to the patient payment portal. If this information seems incorrect please contact us to resolve the matter, Thank You</Say></Response>'
-  // const twiml = new VoiceResponse();
-  const twiml = new VoiceResponse();
 
-  twiml.say('Hi this is Lake City PTs automated phone service stay on the line and we will connect you with a representative');
-  twiml.dial('+12088183015')
-  twiml.dial('+12088507141')
-  console.log(twiml.toString());
+  Patient.find({"number": `${req.body.Caller}`})
+  .then(res =>{
+    if(res.length === 0){
+      const VoiceResponse = require('twilio').twiml.VoiceResponse;
+      const twiml = new VoiceResponse();
+      twiml.say(`Hi this is Lake City PT billing departments automated phone service please stay on the line and we will try an connect you with a representative. If we cannot connect you with a representative please leave us a message and we'll get back to you as soon as possible, or go to lake-city-pt.com to find out more information. Thank You`);
+      const dial = twiml.dial();
+      dial.number(process.env.PHONE_ONE)
+      dial.number(process.env.PHONE_TWO)
+      dial.number(process.env.PHONE_THREE)
+      dial.number(process.env.TEXT_NUMBER)
+      response.type('text/xml');
+      response.send(twiml.toString())
+      console.log(twiml.toString());
+    }else{
+      console.log(res[0].name)
+      const VoiceResponse = require('twilio').twiml.VoiceResponse;
+      const twiml = new VoiceResponse();
+      twiml.say(`Hi ${res[0].name} thank you for giving us a callback, This is Lake City Physical Therapy billing departments automated phone service please stay on the line and we will try an connect you with a representative. If we cannot connect you with a representative please leave us a message and we'll get back to you as soon as possible. Thank You`);
+      const dial = twiml.dial();
+      dial.number(process.env.PHONE_ONE)
+      dial.number(process.env.PHONE_TWO)
+      dial.number(process.env.PHONE_THREE)
+      dial.number(process.env.TEXT_NUMBER)
+      response.type('text/xml');
+      response.send(twiml.toString())
+      console.log(twiml.toString());
+    }
 
-  // twiml.say(`Hi Kyle this is an automated phone message from Lake City Physical Therapy just letting you know that you have an outstanding balance. Please at your earliest convience give our  billing department a call at 208-667-1988 or visit our website at lake-city-pt.com and navigate to the patient payment portal. If this information seems incorrect please contact us to resolve the matter, Thank You and have a nice day.`);
 
-  // // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(twiml.toString());
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+
 });
 
 
 app.post('/add', (req, res)=>{
+
+  console.log(req.body)
 
   const patient = new Patient({
     ...req.body
